0b87629ebffc094648834d9484dcd76a261f61e8
Philbert Malulu <philbertmaluu96@gmail.com>
2024-08-10 21:38:36 +0300

    home page design

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.add_to_photos,
+              size: 90,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Home page',
+              style: TextStyle(fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+
+  const MyButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(25),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 253, 253, 253),
+            borderRadius: BorderRadius.circular(8)),
+        child: Center(
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
